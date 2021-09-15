dea7cc85f3706c8ee5442ce07f7425f124614a4c
Kohyuckjoon <88864152+Kohyuckjoon@users.noreply.github.com>
2021-09-15 14:52:24 +0900

    Update GitTest.java
    [원격리포지토리] 테스트

--- src/test/http/GitTest.java
+++ src/test/http/GitTest.java
@@ -4,4 +4,5 @@ public class GitTest {
 	
 	private static final String DESC = "GIT TEST용 클래스입니다.";
 	private static final String CREDENTIAL = "GIT 이증 테스트!";
+	private static final String GITHUB = "원격리포지토리";
 }
