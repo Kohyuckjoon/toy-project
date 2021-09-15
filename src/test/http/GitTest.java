77d4c9ffd8ddf9314ae5931e5170fb84c3458ccf
akhj123akhj <akhj123akhjK@gmail.com>
2021-09-15 14:57:41 +0900

    [고혁준] DEV 브랜치 병합 및 푸시

--- src/test/http/GitTest.java
+++ src/test/http/GitTest.java
@@ -5,4 +5,5 @@ public class GitTest {
 	private static final String DESC = "GIT TEST용 클래스입니다.";
 	private static final String CREDENTIAL = "GIT 이증 테스트!";
 	private static final String GITHUB = "원격리포지토리";
+	private static final String DEV = "DEV 브래치에서 작업";
 }
