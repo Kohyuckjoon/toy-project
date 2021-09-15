4c85c3b7d67f91e398384a77219b38cde418d378
akhj123akhj <akhj123akhjK@gmail.com>
2021-09-15 14:49:35 +0900

    [고혁준] test

--- src/test/http/GitTest.java
+++ src/test/http/GitTest.java
@@ -2,5 +2,6 @@ package test.http;
 
 public class GitTest {
 	
-	private static final String DESC = "GIT TEST용 클래스입니다..";
+	private static final String DESC = "GIT TEST용 클래스입니다.";
+	private static final String CREDENTIAL = "GIT 이증 테스트!";
 }
