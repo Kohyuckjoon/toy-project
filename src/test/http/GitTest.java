8afec938d22b6a06c4b917a4f1abee135c29dc53
Kohyuckjoon <88864152+Kohyuckjoon@users.noreply.github.com>
2021-09-15 15:09:49 +0900

    Conflict 2차 테스트

--- src/test/http/GitTest.java
+++ src/test/http/GitTest.java
@@ -2,10 +2,10 @@ package test.http;
 
 public class GitTest {
 	
-	private static final String DESC = "GIT TEST용 클래스입니다.";
+	private static final String DESC = "1GIT TEST용 클래스입니다.";
 	private static final String CREDENTIAL = "GIT 이증 테스트!";
-	private static final String GITHUB = "원격리포지토리";
+	private static final String GITHUB = "3원격리포지토리";
 	private static final String LOCALCONFLICT = "local에서 Conflict 테스트";	
 	private static final String CONFLICT = "Conflict";
-	private static final String DEV = "DEV 브래치에서 작업";
+	private static final String DEV = "5DEV 브래치에서 작업";
 }
